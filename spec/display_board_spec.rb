c650bd838931448e6e513bd3408285e038c314a9
deedee medina <deedee.medinacorp@gmail.com>
2020-02-02 19:19:23 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -186,7 +186,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      # expect(true).to be(true)
+      expect(true).to be(true)
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
@@ -194,7 +194,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[4]).to eq(" O | O | O ")
 
     end
-    
+
   end
 end
 # end
